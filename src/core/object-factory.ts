2c1c2d9378562d3af819fecc6b77b17393425bce
Kunal Puri <kunal.puri@stonewain.com>
2018-05-15 19:37:51 +0530

    neat-di updation

--- src/core/object-factory.ts
+++ src/core/object-factory.ts
@@ -2,7 +2,7 @@ export interface ObjectFactory {
     createGlobalObject(args: ObjectFactoryArgs): any;
     createScopedObject(args: ObjectFactoryArgs): any;
     getGlobalObject(name: string, constructorArgs?: Array<any>): any;
-    getScopedObject(name: string, scopeId: string, constructorArgs?: Array<any>): any;
+    getScopedObject(name: string, scopeId?: string, constructorArgs?: Array<any>): any;
     getObject(name: string, scopeId: string, constructorArgs?: Array<any>): any;
     addToNameClassMap(name: string, className: Newable): void;
 }
