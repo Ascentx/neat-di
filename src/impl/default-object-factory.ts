86bdc1178b97abe8acedca2b3c6c126a3e60f782
Amrinder Singh <amsingh@stonewain.com>
2019-04-23 03:18:33 +0530

    Added support for topological dependency resolution.

--- src/impl/default-object-factory.ts
+++ src/impl/default-object-factory.ts
@@ -1,16 +1,27 @@
-import { ObjectFactory, ObjectFactoryArgs, Newable} from "../core/object-factory";
+import { ObjectFactory, ObjectFactoryArgs, Newable, TargetMeta} from "../core/object-factory";
+import * as tsort from "toposort";
 
 export class DefaultObjectFactory implements ObjectFactory {
 
     private static _instance: ObjectFactory = new DefaultObjectFactory();
     private _globalObjectMap: {[key: string]: any} = {};
     private _contextObjectMap: {[key: string]: {[key: string]: any}} = {};
-    private static _nameClassMap: {[key: string]: Newable | any} = {};
-
+    private static _nameClassMap: {[key: string]: {
+        clazz: Newable;
+        required: boolean;
+    }} = {};
+    private static _targetClasses: Array<Object> = [];
+    private static _targetDeps: Array<Array<TargetMeta>> = [];
     constructor() {
         if (DefaultObjectFactory._instance) {
             throw "Injector is singleton can't call new on Injector, Use getInstance Instead.";
         }
+    }
+
+    public init() {
+        let graph = this._buildTargetGraph();
+        let sortedDeps =  tsort(graph).reverse();
+        this._initializeTargetProperties(sortedDeps);
     }
 
     public static getInstance() {
@@ -44,11 +55,14 @@ export class DefaultObjectFactory implements ObjectFactory {
         return this._contextObjectMap[args.scopeId][args.name];
     }
 
-    public addToNameClassMap(name: string, className: Newable): void {
+    public addToNameClassMap(name: string, className: Newable, isRequired?: boolean): void {
         if (DefaultObjectFactory._nameClassMap.hasOwnProperty(name)) {
             throw 'Object with name ' + name + ' is already registered';
         } else {
-            DefaultObjectFactory._nameClassMap[name] = className;
+            DefaultObjectFactory._nameClassMap[name] = {
+                clazz: className,
+                required: isRequired
+            };
         }
     }
 
@@ -57,7 +71,7 @@ export class DefaultObjectFactory implements ObjectFactory {
             name: name,
             scopeId: scopeId,
             constructorArgs: constructorArgs,
-            objectClass: DefaultObjectFactory._nameClassMap[name]
+            objectClass: DefaultObjectFactory._nameClassMap[name].clazz
         });
     }
 
@@ -106,7 +120,7 @@ export class DefaultObjectFactory implements ObjectFactory {
         return this.createGlobalObject({
             name: name,
             constructorArgs: constructorArgs,
-            objectClass: DefaultObjectFactory._nameClassMap[name]
+            objectClass: DefaultObjectFactory._nameClassMap[name].clazz
         });
     }
 
@@ -133,4 +147,78 @@ export class DefaultObjectFactory implements ObjectFactory {
             return this.getScopedObject(name, scopeId, constructorArgs);
         }
     }
+
+    public addToTargetMap(meta: TargetMeta) {
+        let index = DefaultObjectFactory._targetClasses.indexOf(meta.target);
+
+        if (index === -1 ) {
+            DefaultObjectFactory._targetClasses.push(meta.target);
+            DefaultObjectFactory._targetDeps.push([meta]);
+        } else {
+            DefaultObjectFactory._targetDeps[index].push(meta);
+        }
+    }
+
+    private _buildTargetGraph(): Array<Array<Number>> {
+        let deps = DefaultObjectFactory._targetDeps,
+            graph = [],
+            own = [],
+            dep, i, j, targetName;
+
+        for (i = 0; i < deps.length; i++) {
+            for (j = 0; j < deps[i].length; j++) {
+                targetName = this._getNameFromTarget(deps[i][j].target.constructor)
+                dep = deps[i][j]['name'];
+                graph[i + j] = [targetName, dep];
+            }
+        }
+
+        return graph;
+    }
+
+    private _initializeTargetProperties(depOrder: Array<string>) {
+        for (let name of depOrder) {
+            if (name) {
+                let ob = this.getObject(name);
+                let deps = this._getDepsOfTarget(name);
+
+                for (let dep of deps) {
+                    ob[dep.key] = this.getObject(dep.name, dep.scopeId, dep.constructorArgs);
+                }
+            }
+        }
+    }
+
+    private _getNameFromTarget(target: Function) {
+        let classes = DefaultObjectFactory._nameClassMap
+
+        for (let key in classes) {
+            if (classes[key].clazz === target) {
+                return key;
+            }
+        }
+    }
+
+    private _getDepsOfTarget(name): Array<TargetMeta> {
+        let target = DefaultObjectFactory._nameClassMap[name],
+            classMap = DefaultObjectFactory._targetClasses,
+            deps = DefaultObjectFactory._targetDeps,
+            i = 0;
+
+        if (target) {
+            for (let clazz of classMap) {
+                if (clazz.constructor === target.clazz) {
+                    return deps[i];
+                }
+                i++;
+            }
+        }
+
+        return [];
+    }
+}
+
+interface TargetMetaInjectableMap {
+    meta: TargetMeta;
+    injectable: Object;
 }
--- test/neat-di.spec.ts
+++ test/neat-di.spec.ts
@@ -1,33 +1,55 @@
+import { MockInjectable6, MockInjectable4, MockInjectable2 } from './mocks/mock-injectable-2';
+import { ObjectFactory } from 'src/core/object-factory';
 import { MockDi } from "./mocks/mock-di";
 import {MockInjectable} from './mocks/mock-injectable';
 import { DefaultObjectFactory } from "src/all";
 
 describe('neat-di', () => {
+    let of: ObjectFactory = DefaultObjectFactory.getInstance();
+
+    beforeAll(() => {
+        of.init();
+    });
+
     it('Should register the injectable', () => {
-        let mdi: MockDi = new MockDi();
+        let mdi: MockDi = of.getObject('MockDi');
         expect(mdi.globalMi1).toBeDefined();
         expect(mdi.globalMi1 instanceof MockInjectable).toBeTruthy();
     });
 
     it('Should give same singleton global on every inject', () => {
-        let mdi: MockDi = new MockDi();
+        let mdi: MockDi = of.getObject('MockDi');
         expect(mdi.globalMi1).toEqual(mdi.globalMi2);
     });
 
     it('Should give different instance on every inject if scoped', () => {
-        let mdi: MockDi = new MockDi();
+        let mdi: MockDi = of.getObject('MockDi');
         expect(mdi.scopedMi1).not.toBe(mdi.scopedMi2);
     });
 
     it('Should give same instance on every inject if scoped and scope id is same', () => {
-        let mdi1: MockDi = new MockDi(),
-            mdi2: MockDi = new MockDi();
+        let mdi1: MockDi = of.getObject('MockDi'),
+            mdi2: MockDi = of.getObject('MockDi');
 
         expect(mdi1.scopedMi1).toBe(mdi2.scopedMi1);
     });
 
     it('Should dynamically give scoped instance', () => {
-        let mdi: MockDi = new MockDi();
-        expect(DefaultObjectFactory.getInstance().getObject('mock-injectable', '2')).toBe(mdi.scopedMi2);
+        let mdi: MockDi = of.getObject('MockDi');
+        expect(of.getObject('mock-injectable', '2')).toBe(mdi.scopedMi2);
     });
-})
+
+    it('Should sort topologically and create instances accordingly', () => {
+        let mdi: MockInjectable6 = of.getObject('mock-injectable-6');
+        expect(mdi).toBeDefined();
+        expect(mdi.prop1).toBeDefined();
+        expect(mdi.prop2).toBeDefined();
+        expect(mdi.prop3).toBeDefined();
+        expect(mdi.prop1.prop).toBeDefined();
+        expect(mdi.prop2.prop).toBeDefined();
+        expect(mdi.prop3.prop2).toBeDefined();
+        expect(mdi.prop3.prop).toBeDefined();
+        expect(mdi.prop3.prop instanceof MockInjectable4).toBeTruthy();
+        expect(mdi.prop3.prop2 instanceof MockInjectable2).toBeTruthy();
+    })
+});
