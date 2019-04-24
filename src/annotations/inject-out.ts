884a9dbddb6e49dfe556662f1aa44a3732950d9e
Amrinder Singh <amsingh@stonewain.com>
2019-04-24 15:22:50 +0530

    Modified InjectOut for getter only.

--- src/annotations/inject-out.ts
+++ src/annotations/inject-out.ts
@@ -2,12 +2,26 @@ import "reflect-metadata";
 import { DefaultObjectFactory } from "../impl/default-object-factory";
 import { ObjectFactory } from "src/core/object-factory";
 
-const injectMetadataKey = Symbol("Inject");
-
 export function InjectOut(name: string, scopeId?: string, constructorArgs?: Array<any>): any {
     return (target: Object, key: string) => {
         let of: ObjectFactory = DefaultObjectFactory.getInstance();
-        target[key] = of.getObject(name, scopeId, constructorArgs);
 
+        const getter = function () {
+            return of.getObject(name, scopeId, constructorArgs);
+        };
+
+        const setter = function (newVal) {
+            throw "Cannot override an injected property."
+        };
+
+        if (delete target[key]) {
+            // Create new property with getter and setter
+            Object.defineProperty(target, key, {
+                get: getter,
+                set: setter,
+                enumerable: true,
+                configurable: true
+            });
+        }
     }
 }
--- test/neat-di-out.spec.ts
+++ test/neat-di-out.spec.ts
@@ -2,6 +2,7 @@ import { ObjectFactory } from 'src/core/object-factory';
 import { MockDiOut } from "./mocks/mock-di-out";
 import { MockInjectable } from './mocks/mock-injectable';
 import { DefaultObjectFactory } from "src/all";
+import { MockInjectable6, MockInjectable4, MockInjectable2 } from './mocks/mock-injectable-2';
 
 describe('neat-di-out', () => {
     let of: ObjectFactory = DefaultObjectFactory.getInstance();
@@ -34,6 +35,20 @@ describe('neat-di-out', () => {
 
     it('Should dynamically give scoped instance', () => {
         let mdi: MockDiOut = new MockDiOut();
-        expect(DefaultObjectFactory.getInstance().getObject('mock-injectable', '2')).toBe(mdi.scopedMi2);
+        expect(of.getObject('mock-injectable', '2')).toBe(mdi.scopedMi2);
     });
+
+    it('Should sort topologically and create instances accordingly', () => {
+        let mdi: MockInjectable6 = new MockDiOut().depependentDi;
+        expect(mdi).toBeDefined();
+        expect(mdi.prop1).toBeDefined();
+        expect(mdi.prop2).toBeDefined();
+        expect(mdi.prop3).toBeDefined();
+        expect(mdi.prop1.prop).toBeDefined();
+        expect(mdi.prop2.prop).toBeDefined();
+        expect(mdi.prop3.prop2).toBeDefined();
+        expect(mdi.prop3.prop).toBeDefined();
+        expect(mdi.prop3.prop instanceof MockInjectable4).toBeTruthy();
+        expect(mdi.prop3.prop2 instanceof MockInjectable2).toBeTruthy();
+    })
 });
